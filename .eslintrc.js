91f12291969fb4f183d1c1305ab2aea3ae8e4387
braydenkilleen <braydenmkilleen@gmail.com>
2020-08-08 00:42:09 +1000

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "node": true,
+        "commonjs": true,
+        "jest": "true"
+    },
+    "extends": [
+        "eslint:recommended", "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended"
+    ],
+    "settings": {
+        "react": {
+          "version": "16.6.1"
+        }
+      },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+        , "jsx-a11y"
+    ],
+    "rules": {
+    }
+};
