781a8cb2696fd5ad257366465ef8070b50383df4
braydenkilleen <braydenmkilleen@gmail.com>
2020-08-08 00:34:47 +1000

    Jest config

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,6 +3,9 @@ import { render } from 'react-testing-library'
 import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 import App from './App'
+import { render } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
 
 describe('App', () => {
     it('Renders without error', () => {
