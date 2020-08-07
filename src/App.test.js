35de81e25c6d796352a7fe0632d38cc4025e9bb9
braydenkilleen <braydenmkilleen@gmail.com>
2020-08-08 02:08:58 +1000

    renamed files and updated packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render } from '@testing-library/react'
 import App from './App'
-import 'jest-dom/extend-expect'
-import 'react-testing-library/cleanup-after-each'
 
 describe('App', () => {
   it('Renders without error', () => {
