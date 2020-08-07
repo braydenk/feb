8e27cf2b5e8341588b0eebb0c47908ebf04f6b60
braydenkilleen <braydenmkilleen@gmail.com>
2020-08-08 00:47:13 +1000

    Added husky

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { render } from 'react-testing-library'
-import 'jest-dom/extend-expect'
-import 'react-testing-library/cleanup-after-each'
 import App from './App'
-import { render } from 'react-testing-library'
 import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
 
